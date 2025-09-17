325cec0e3736edfb7011a41af19ebdb9e95e3036
rozmarymethod@gmail.com <rozmarymethod@gmail.com>
2025-09-17 14:25:50 +0200

    feat(reminder): add Reminder entity with fields and annotations

--- src/main/java/com/_2/Backend/reminder/Reminder.java
+++ src/main/java/com/_2/Backend/reminder/Reminder.java
@@ -1,4 +1,30 @@
 package com._2.Backend.reminder;
 
-public class Reminder {
-}
+import com._2.Backend.medication.Medication;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalTime;
+
+    @Entity
+    @Table(name = "reminders")
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Data
+    @Builder
+    public class Reminder {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        private LocalTime time;
+
+        private Boolean active = true;
+
+        private Boolean taken = false;
+
+        @ManyToOne(optional = false)
+        @JoinColumn(name = "medication_id")
+        private Medication medication;
+    }
