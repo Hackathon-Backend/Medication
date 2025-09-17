d78708fe35a015f147f37128b861d54747c06517
Alejandro-Cuesta <janittoss@hotmail.com>
2025-09-17 16:48:01 +0200

    feat:commit before made pull from dev

--- src/main/java/com/_2/Backend/medication/MedicationRepository.java
+++ src/main/java/com/_2/Backend/medication/MedicationRepository.java
@@ -6,37 +6,22 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalTime;
 import java.util.List;
 
-//Interfaz que define el repositorio para la entidad Medication extiende JpaRepository para heredar métodos CRUD automaticamente//
+ 
  
 @Repository
 public interface MedicationRepository extends JpaRepository<Medication, Long> {
-    
-    //Metodo Principal medicación por tomar//
 
     List<Medication> findByStatusAndActiveTrueOrderByTimeToTakeAsc(Medication.Status status);
 
-    //Metodo para la medicacion ya tomada//
-
-     List<Medication> findByStatusOrderByTimeToTakeDesc(Medication.Status status);
-
-    //Método para buscar medicamentos activos Spring Data JPA genera automáticamente la consulta SQL correspondiente//
+    List<Medication> findByStatusOrderByTimeToTakeDesc(Medication.Status status);
 
     List<Medication> findByActive(boolean active);
-
-    //Método para buscar medicamentos por estado (PENDING o TAKEN)//
      
     List<Medication> findByStatus(Medication.Status status);
-
-    //Método para buscar medicamentos que se toman a una hora específica//
      
     List<Medication> findByTimeToTake(LocalTime timeToTake);
 
-    
-    //Metodo para buscar medicamentos por nombre exacto//
-
     List<Medication> findByName(String name);
-
-    //Buscar medicamentos por nombre parcial, ignorando mayúsculas/minúsculas//
 
     List<Medication> findByNameContainingIgnoreCase(String name);
 }
