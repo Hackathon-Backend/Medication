8dc021ad96ffe00b1e3041b3c094ce9b6f84cb1f
rozmarymethod@gmail.com <rozmarymethod@gmail.com>
2025-09-17 14:26:50 +0200

    feat(reminder): create ReminderRepository extending JpaRepository

--- src/main/java/com/_2/Backend/reminder/ReminderRepository.java
+++ src/main/java/com/_2/Backend/reminder/ReminderRepository.java
@@ -1,4 +1,9 @@
 package com._2.Backend.reminder;
 
-public interface ReminderRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReminderRepository extends JpaRepository<Reminder, Long> {
+    List<Reminder> findByActiveTrue();
 }
